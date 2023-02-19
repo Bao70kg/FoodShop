dd7e082cdf0c18bfe6ab7557958385fecd3131e4
Bao70kg <117332792+Bao70kg@users.noreply.github.com>
2023-02-19 16:54:36 +0700

    dbd

--- screen/bottom_tab/Cart.js
+++ screen/bottom_tab/Cart.js
@@ -3,9 +3,133 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+   <View style={styles.container}>
+            <View style={styles.Arow}>
+                <Image
+                    source={require('../media/images/IconArrow.png')}
+                />
+                <Text style={styles.textcart}>Cart</Text>
+                <View></View>
+              
+         
+
+
+            </View>
+
+        
+
+            <View style={styles.ViewTong}>
+                <View style={styles.imgtext} >
+                    <Image
+                        source={require('../media/images/tao.png')}
+                    />
+                    <Text style={styles.RedApple}>Red Apple</Text>
+                </View>
+
+                <View style={styles.ViewCongtru}>
+                    <View style={styles.tru}>
+                        <Image style={styles.imgtru}
+                            source={require('../media/images/minus.png')}
+                        />
+                    </View>
+                    <Text>2</Text>
+                    <View style={styles.cong}>
+                        <Image
+                            source={require('../media/images/cong.png')}
+                        />
+                    </View>
+                </View>
+            </View>
+            <View style={styles.ViewGiaGia}>
+                <Text style={styles.textGia}>$4,99</Text>
+                <Text style={styles.kg}>kg</Text>
+            </View>
+            <View style={styles.gach}></View>
+
+            <View style={styles.ViewTong}>
+                <View style={styles.imgtext} >
+                    <Image
+                        source={require('../media/images/chuoi.png')}
+                    />
+                    <Text style={styles.RedApple}>Red Apple</Text>
+                </View>
+
+                <View style={styles.ViewCongtru}>
+                    <View style={styles.tru}>
+                    <Image style={styles.imgtru}
+                            source={require('../media/images/minus.png')}
+                        />
+                    </View>
+                    <Text>2</Text>
+                    <View style={styles.cong}>
+                    source={require('../media/images/cong.png')}
+                    </View>
+                </View>
+            </View>
+            <View style={styles.ViewGiaGia}>
+                <Text style={styles.textGia}>$4,99</Text>
+                <Text style={styles.kg}>kg</Text>
+            </View>
+            <View style={styles.gach}></View>
+
+
+            <View style={styles.ViewTong}>
+                <View style={styles.imgtext} >
+                    <Image
+                        source={require('../media/images/bo.png')}
+                    />
+                    <Text style={styles.RedApple}>Red Apple</Text>
+                </View>
+
+                <View style={styles.ViewCongtru}>
+                    <View style={styles.tru}>
+                    <Image style={styles.imgtru}
+                            source={require('../media/images/minus.png')}
+                        />
+                    </View>
+                    <Text>2</Text>
+                    <View style={styles.cong}>
+                    source={require('../media/images/cong.png')}
+                    </View>
+                </View>
+            </View>
+            <View style={styles.ViewGiaGia}>
+                <Text style={styles.textGia}>$4,99</Text>
+                <Text style={styles.kg}>kg</Text>
+            </View>
+            <View style={styles.gach}></View>
+
+            <View style={styles.ViewTong}>
+                <View style={styles.imgtext} >
+                    <Image
+                        source={require('../media/images/ca.png')}
+                    />
+                    <Text style={styles.RedApple}>Red Apple</Text>
+                </View>
+
+                <View style={styles.ViewCongtru}>
+                    <View style={styles.tru}>
+                    <Image style={styles.imgtru}
+                            source={require('../media/images/minus.png')}
+                        />
+                    </View>
+                    <Text>2</Text>
+                    <View style={styles.cong}>
+                    source={require('../media/images/cong.png')}
+                    </View>
+                </View>
+            </View>
+            <View style={styles.ViewGiaGia}>
+                <Text style={styles.textGia}>$4,99</Text>
+                <Text style={styles.kg}>kg</Text>
+            </View>
+            <View style={styles.gach}></View>
+            <View style={styles.ViewPre}>
+                <Pressable style={styles.Presnext} >
+                    <Text style={styles.next}>CheckOut</Text>
+                </Pressable>
+            </View>
+        </View>
   )
 }
 
